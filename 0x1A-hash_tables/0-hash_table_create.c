bb1fc8333f5928c4ab0f1696e3ca246c1fc87f7a
DNachinab <dnachinab@gmail.com>
2024-02-16 18:06:43 +0000

    0x1A. C - Hash tables

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Creates a hash table.
+ * @size: size of array.
+ *
+ * Return: If an error occurs - NULL.
+ *         else - a pointer to the new hash table.
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hsht;
+	unsigned long int i;
+
+	hsht = malloc(sizeof(hash_table_t));
+	if (hsht == NULL)
+		return (NULL);
+
+	hsht->size = size;
+	hsht->array = malloc(sizeof(hash_node_t *) * size);
+	if (hsht->array == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		hsht->array[i] = NULL;
+
+	return (hsht);
+}
